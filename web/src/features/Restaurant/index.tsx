4088357cff64d422ae8c018a815402a58e21a2d4
centerRyo <abc.atrn@gmail.com>
2024-08-30 12:56:29 +0900

    他のページにも500エラーハンドリングを適用

--- web/src/features/Restaurant/index.tsx
+++ web/src/features/Restaurant/index.tsx
@@ -2,6 +2,7 @@ import { api } from '@/lib/swagger-client';
 import { Button, Flex, Heading, Skeleton } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import useSWR from 'swr';
+import { ErrorSafePage } from '../Error';
 import Ingredients from './Ingredients';
 import { useHandler } from './hooks';
 import { FormValues } from './index.d';
@@ -12,8 +13,9 @@ type Props = {
 };
 
 export const Restaurant = ({ restaurantId }: Props) => {
-  const { data, isLoading } = useSWR(`/restaurants/${restaurantId}`, () =>
-    api.restaurants.restaurantsControllerFindOne(restaurantId)
+  const { data, error, isLoading } = useSWR(
+    `/restaurants/${restaurantId}`,
+    () => api.restaurants.restaurantsControllerFindOne(restaurantId)
   );
   const restaurant = data?.data;
 
@@ -24,50 +26,52 @@ export const Restaurant = ({ restaurantId }: Props) => {
   });
 
   return (
-    <div className={styles.container}>
-      {!isLoading ? (
-        <Heading mb={9}>{restaurant?.name}</Heading>
-      ) : (
-        <Skeleton height='2rem' mb={8} />
-      )}
+    <ErrorSafePage error={error}>
+      <div className={styles.container}>
+        {!isLoading ? (
+          <Heading mb={9}>{restaurant?.name}</Heading>
+        ) : (
+          <Skeleton height='2rem' mb={8} />
+        )}
 
-      <section className={styles.explanation}>
-        <div className={styles.wrapper}>
-          <p>アレルギー物質を選択してください</p>
-          <p>(複数選択可)</p>
-          <p>
-            (選択した物質を
-            <span className={styles.unused}>使用していない</span>
-            メニューが表示されます)
-          </p>
-        </div>
-      </section>
+        <section className={styles.explanation}>
+          <div className={styles.wrapper}>
+            <p>アレルギー物質を選択してください</p>
+            <p>(複数選択可)</p>
+            <p>
+              (選択した物質を
+              <span className={styles.unused}>使用していない</span>
+              メニューが表示されます)
+            </p>
+          </div>
+        </section>
 
-      <Ingredients register={register} />
+        <Ingredients register={register} />
 
-      <Flex justifyContent='center'>
-        <Button
-          size='lg'
-          colorScheme='green'
-          width='100%'
-          maxWidth='30rem'
-          onClick={handleClickSearch}
-        >
-          検索
-        </Button>
-      </Flex>
-      <Flex justifyContent='center' mt={8}>
-        <Button
-          size='lg'
-          colorScheme='green'
-          variant='outline'
-          width='100%'
-          maxWidth='30rem'
-          onClick={handleBack}
-        >
-          店舗を再検索する
-        </Button>
-      </Flex>
-    </div>
+        <Flex justifyContent='center'>
+          <Button
+            size='lg'
+            colorScheme='green'
+            width='100%'
+            maxWidth='30rem'
+            onClick={handleClickSearch}
+          >
+            検索
+          </Button>
+        </Flex>
+        <Flex justifyContent='center' mt={8}>
+          <Button
+            size='lg'
+            colorScheme='green'
+            variant='outline'
+            width='100%'
+            maxWidth='30rem'
+            onClick={handleBack}
+          >
+            店舗を再検索する
+          </Button>
+        </Flex>
+      </div>
+    </ErrorSafePage>
   );
 };
