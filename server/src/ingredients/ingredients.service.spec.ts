49edc10ef56ff5251cfd552eca0a4a0e0fbffa66
centerRyo <abc.atrn@gmail.com>
2024-09-04 15:14:21 +0900

    全ての原材料を取得できる

--- server/src/ingredients/ingredients.service.spec.ts
+++ server/src/ingredients/ingredients.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { PrismaService } from '../prisma.service';
+import { IngredientsModule } from './ingredients.module';
 import { IngredientsService } from './ingredients.service';
 
 describe('IngredientsService', () => {
   let service: IngredientsService;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IngredientsService],
+      imports: [IngredientsModule, AppModule],
     }).compile();
 
     service = module.get<IngredientsService>(IngredientsService);
+
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterAll(async () => {
+    await prisma.ingredients.deleteMany();
+  });
+
+  describe('findAll', () => {
+    beforeAll(async () => {
+      await prisma.ingredients.createMany({
+        data: [
+          {
+            name: 'ingredient1',
+            pic: 'pic1',
+          },
+          {
+            name: 'ingredient2',
+            pic: 'pic2',
+          },
+          {
+            name: 'ingredient3',
+            pic: 'pic3',
+          },
+        ],
+      });
+    });
+    it('全ての原材料を取得できる', async () => {
+      const res = await service.findAll();
+
+      expect(res.length).toBe(3);
+      expect(res[0].name).toBe('ingredient1');
+    });
   });
 });
