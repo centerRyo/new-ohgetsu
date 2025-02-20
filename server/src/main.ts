c5ed144e365747073c6da8c533d41e3be3acd0c6
centerRyo <abc.atrn@gmail.com>
2025-02-20 15:39:25 +0900

    大きなファイルがリクエストにある場合でも受け付けるようにする

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,16 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Body Parserのサイズ制限を増やすことで
+  // 大きなファイルがリクエストにある場合でも受け付けるようにする
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 8080;
