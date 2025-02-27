c465d33c324dd4ede76ecc51bed4202aacfcc9ff
centerRyo <abc.atrn@gmail.com>
2025-02-27 13:09:28 +0900

    不要なconsoleを削除

--- server/src/restaurants/restaurants.controller.ts
+++ server/src/restaurants/restaurants.controller.ts
@@ -70,7 +70,6 @@ export class RestaurantsController {
     @Body() data: CreateRestaurantDto,
     @UploadedFile() pic?: Express.Multer.File
   ): Promise<RestaurantDto> {
-    console.log('🔴🔴🔴🔴🔴🔴🔴', process.env.SAKURA_OBJECT_STORAGE_ENDPOINT);
     const restaurant = await this.restaurantsService.create(data, pic);
 
     return new RestaurantDto(restaurant);
