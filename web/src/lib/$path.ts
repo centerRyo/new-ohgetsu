def472e3ca2b3d860088badd0ddca5fdb8ab2660
centerRyo <abc.atrn@gmail.com>
2025-03-25 16:33:07 +0900

    fix format

--- web/src/lib/$path.ts
+++ web/src/lib/$path.ts
@@ -1,10 +1,13 @@
-import type { OptionalQuery as OptionalQuery_qbbltk } from '../app/menus/page';
 import type { OptionalQuery as OptionalQuery_gsgao6 } from '../app/menus/[id]/page';
+import type { OptionalQuery as OptionalQuery_qbbltk } from '../app/menus/page';
 import type { OptionalQuery as OptionalQuery_1rgxedo } from '../app/restaurants/page';
 
 const buildSuffix = (url?: {
-  query?: Record<string, string | number | boolean | Array<string | number | boolean>>,
-  hash?: string
+  query?: Record<
+    string,
+    string | number | boolean | Array<string | number | boolean>
+  >;
+  hash?: string;
 }) => {
   const query = url?.query;
   const hash = url?.hash;
@@ -16,9 +19,7 @@ const buildSuffix = (url?: {
 
     Object.entries(query).forEach(([key, value]) => {
       if (Array.isArray(value)) {
-        value.forEach((item) =>
-          params.append(key, String(item))
-        );
+        value.forEach((item) => params.append(key, String(item)));
       } else {
         params.set(key, String(value));
       }
@@ -30,29 +31,62 @@ const buildSuffix = (url?: {
 };
 
 export const pagesPath = {
-  'admin': {
-    'restaurants': {
+  admin: {
+    restaurants: {
       _id: (id: string | number) => ({
-        'menus': {
-          $url: (url?: { hash?: string }) => ({ pathname: '/admin/restaurants/[id]/menus' as const, query: { id }, hash: url?.hash, path: `/admin/restaurants/${id}/menus${buildSuffix(url)}` })
-        }
+        menus: {
+          $url: (url?: { hash?: string }) => ({
+            pathname: '/admin/restaurants/[id]/menus' as const,
+            query: { id },
+            hash: url?.hash,
+            path: `/admin/restaurants/${id}/menus${buildSuffix(url)}`,
+          }),
+        },
       }),
-      $url: (url?: { hash?: string }) => ({ pathname: '/admin/restaurants' as const, hash: url?.hash, path: `/admin/restaurants${buildSuffix(url)}` })
-    }
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/admin/restaurants' as const,
+        hash: url?.hash,
+        path: `/admin/restaurants${buildSuffix(url)}`,
+      }),
+    },
   },
-  'menus': {
+  menus: {
     _id: (id: string | number) => ({
-      $url: (url?: { query?: OptionalQuery_gsgao6, hash?: string }) => ({ pathname: '/menus/[id]' as const, query: { id, ...url?.query }, hash: url?.hash, path: `/menus/${id}${buildSuffix(url)}` })
+      $url: (url?: { query?: OptionalQuery_gsgao6; hash?: string }) => ({
+        pathname: '/menus/[id]' as const,
+        query: { id, ...url?.query },
+        hash: url?.hash,
+        path: `/menus/${id}${buildSuffix(url)}`,
+      }),
     }),
-    $url: (url?: { query?: OptionalQuery_qbbltk, hash?: string }) => ({ pathname: '/menus' as const, query: url?.query, hash: url?.hash, path: `/menus${buildSuffix(url)}` })
+    $url: (url?: { query?: OptionalQuery_qbbltk; hash?: string }) => ({
+      pathname: '/menus' as const,
+      query: url?.query,
+      hash: url?.hash,
+      path: `/menus${buildSuffix(url)}`,
+    }),
   },
-  'restaurants': {
+  restaurants: {
     _id: (id: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/restaurants/[id]' as const, query: { id }, hash: url?.hash, path: `/restaurants/${id}${buildSuffix(url)}` })
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/restaurants/[id]' as const,
+        query: { id },
+        hash: url?.hash,
+        path: `/restaurants/${id}${buildSuffix(url)}`,
+      }),
     }),
-    $url: (url?: { query?: OptionalQuery_1rgxedo, hash?: string }) => ({ pathname: '/restaurants' as const, query: url?.query, hash: url?.hash, path: `/restaurants${buildSuffix(url)}` })
+    $url: (url?: { query?: OptionalQuery_1rgxedo; hash?: string }) => ({
+      pathname: '/restaurants' as const,
+      query: url?.query,
+      hash: url?.hash,
+      path: `/restaurants${buildSuffix(url)}`,
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash, path: `/${buildSuffix(url)}` })
+  $url: (url?: { hash?: string }) => ({
+    pathname: '/' as const,
+    hash: url?.hash,
+    path: `/${buildSuffix(url)}`,
+  }),
 };
 
 export type PagesPath = typeof pagesPath;
