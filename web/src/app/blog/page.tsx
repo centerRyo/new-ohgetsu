1f8ca458dd1833a51ad330405b23a6b82f82059a
centerRyo <abc.atrn@gmail.com>
2026-01-07 13:05:05 +0900

    metadata の追加

--- web/src/app/blog/page.tsx
+++ web/src/app/blog/page.tsx
@@ -5,6 +5,29 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: `アレルギー外食ガイド | ${SERVICE_NAME}`,
   description: 'アレルギー外食のヒント・ohgetsuの使い方などを発信します',
+
+  alternates: {
+    canonical: 'https://ohgetsu.com/blog',
+  },
+
+  openGraph: {
+    title: `アレルギー外食ガイド | ${SERVICE_NAME}`,
+    description: 'アレルギー外食のヒント・ohgetsuの使い方などを発信します',
+    url: 'https://ohgetsu.com/blog',
+    siteName: SERVICE_NAME,
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: `アレルギー外食ガイド | ${SERVICE_NAME}`,
+    description: 'アレルギー外食のヒント・ohgetsuの使い方などを発信します',
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 const Page = () => <IndexPage />;
