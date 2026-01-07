c3a3beb069ed77692282364ba7e46e71e111b94a
centerRyo <abc.atrn@gmail.com>
2026-01-07 13:14:46 +0900

    metadata追加

--- web/src/app/blog/[slug]/page.tsx
+++ web/src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { SERVICE_NAME } from '@/constants';
 import { getAllSlugs, getPostBySlug } from '@/features/blog/api/repository';
 import { PostPage } from '@/features/blog/components/pages/PostPage';
+import { Metadata } from 'next';
 
 type Props = {
   params: { slug: string };
@@ -10,16 +11,57 @@ export const generateStaticParams = () => {
   return getAllSlugs().map((slug) => ({ slug }));
 };
 
-export const generateMetadata = async ({ params }: Props) => {
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const slug = params.slug;
+
   try {
-    const post = getPostBySlug(params.slug);
+    const post = getPostBySlug(slug);
+
+    const title = `${post.frontmatter.title} | アレルギー外食ガイド | ${SERVICE_NAME}`;
+    const description = post.frontmatter.description ?? '';
+
+    const canonicalUrl = `https://ohgetsu.com/blog/${slug}`;
 
     return {
-      title: `${post.frontmatter.title} | ブログ | ${SERVICE_NAME}`,
-      description: post.frontmatter.description ?? '',
+      title,
+      description,
+
+      alternates: {
+        canonical: canonicalUrl,
+      },
+
+      robots: {
+        index: true,
+        follow: true,
+      },
+
+      openGraph: {
+        title,
+        description,
+        url: canonicalUrl,
+        siteName: SERVICE_NAME,
+        type: 'article',
+        publishedTime: post.frontmatter.date,
+        tags: post.frontmatter.tags,
+      },
+
+      twitter: {
+        title,
+        description,
+      },
+
+      keywords: post.frontmatter.tags,
     };
   } catch {
-    return { title: `Not Found | ${SERVICE_NAME}` };
+    const notFoundUrl = `https://ohgetsu.com/blog/${slug}`;
+
+    return {
+      title: `Not Found | ${SERVICE_NAME}`,
+      alternates: { canonical: notFoundUrl },
+      robots: { index: false, follow: false },
+    };
   }
 };
 
